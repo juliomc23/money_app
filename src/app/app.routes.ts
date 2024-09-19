d3306384fa5ed9de5a4d5c7ec993940517e85f11
julio <julito2302mc@gmail.com>
2024-09-19 11:56:06 +0200

    feat: add home and accounts for router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'accounts',
+    loadComponent: () =>
+      import('./accounts/accounts.component').then((m) => m.AccountsComponent),
+  },
+];
